11d4bc0575d99fa6418c36868d0875cd6520650c
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2021-10-14 09:06:26 +0200

    [:hammer: REFACTOR] Cleanup code

--- src/GoogleTagManager.php
+++ src/GoogleTagManager.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @noinspection PhpUnused
+ * @noinspection UnknownInspectionInspection
+ */
+
 namespace TomLutzenberger\GoogleTagManager;
 
 use Yii;
@@ -16,6 +21,7 @@ use yii\web\View;
  */
 class GoogleTagManager extends Widget
 {
+
     /**
      * @var string The id of the container
      */
@@ -36,17 +42,25 @@ class GoogleTagManager extends Widget
         }
 
         $view = $this->getView();
-        $view->registerJs("(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        $view->registerJs(
+            <<<SCRIPT
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','{$this->gtmId}');", View::POS_HEAD);
+})(window,document,'script','dataLayer','$this->gtmId');
+SCRIPT
+            ,
+            View::POS_HEAD
+        );
 
-        return Html::tag('noscript', Html::tag('iframe', '', [
+        $gtmFrame = Html::tag('iframe', '', [
             'src'    => 'https://www.googletagmanager.com/ns.html?id=' . $this->gtmId,
             'height' => '0',
             'width'  => '0',
             'style'  => 'display:none;visibility:hidden',
-        ]));
+        ]);
+
+        return Html::tag('noscript', $gtmFrame);
     }
 }
